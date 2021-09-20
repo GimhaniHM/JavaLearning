9e8d7816a9c254e223e1d82ac7274c7a21be9c50
Gimahni Harshika <89870885+GimhaniHM@users.noreply.github.com>
2021-09-20 18:25:26 +0500

    Update IntegerMethod1.java

--- IntegerPackage/IntegerMethod1.java
+++ IntegerPackage/IntegerMethod1.java
@@ -10,6 +10,11 @@ public class IntegerMethod1 {
         System.out.println(a.intValue());   //returns integer value of a
         System.out.println(b.doubleValue());   //returns double value of b
         System.out.println(c.floatValue());   //returns float value of c
-
+        System.out.println(Integer.parseInt("241356"));
+        System.out.println(Integer.compare(a,d));   //returns 0 if equals
+        System.out.println(Integer.compare(d,b));    //returns 1 if d > b
+        System.out.println(Integer.compare(b,a));    //returns -1 if b < a
+        System.out.println(a.equals(d));    //returns true if a equals d else returns false
+        System.out.println(a.equals(c));
     }
 }
